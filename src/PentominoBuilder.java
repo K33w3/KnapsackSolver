63b271c1c507fc5db3fe00051d79272d80b8d75d
Octavian Covalciuc <70703557+Octavian-Covalciuc@users.noreply.github.com>
2023-10-08 14:37:56 +0200

    Implemented Brute force

--- src/PentominoBuilder.java
+++ src/PentominoBuilder.java
@@ -3,11 +3,12 @@
  * @version 2022.0
  */
 
- import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 //import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * This class contains all the methods that you may need to start developing your project together with the representation of the pentomino's pieces
@@ -17,13 +18,13 @@ public class PentominoBuilder {
     //All basic pentominoes that will be rotated and flipped
     private static int[][][] basicDatabase = {
             {
-            	// pentomino representation X
+                    // pentomino representation X
                     {0,1,0},
                     {1,1,1},
                     {0,1,0}
             },
             {
-            	// pentomino representation I
+                    // pentomino representation I
                     {1},
                     {1},
                     {1},
@@ -31,69 +32,62 @@ public class PentominoBuilder {
                     {1}
             },
             {
-            	// pentomino representation Z
+                    // pentomino representation Z
                     {0,1,1},
                     {0,1,0},
                     {1,1,0}
             },
             {
-            	// pentomino representation T
+                    // pentomino representation T
                     {1,1,1},
                     {0,1,0},
                     {0,1,0}
             },
             {
-            	// pentomino representation U
+                    // pentomino representation U
                     {1,1},
                     {1,0},
                     {1,1}
             },
             {
-            	// pentomino representation V
+                    // pentomino representation V
                     {1,1,1},
                     {1,0,0},
                     {1,0,0}
             },
             {
-            	// pentomino representation W
+                    // pentomino representation W
                     {0,0,1},
                     {0,1,1},
                     {1,1,0}
             },
             {
-            	// pentomino representation Y
+                    // pentomino representation Y
                     {1,0},
                     {1,1},
                     {1,0},
                     {1,0}
             },
             {
-            	// pentomino representation L
+                    // pentomino representation L
                     {1,0},
                     {1,0},
                     {1,0},
                     {1,1}
             },
             {
-            	//
-        		//
-        		// TODO: Implement pentomino representation P
-        		//
-        		//
+                    {1,1},
+                    {1,1},
+                    {1,0}
             },
             {
-            	//
-        		//
-        		// TODO: Implement pentomino representation N
-        		//
-        		//
+                    {1,1,0,0},
+                    {0,1,1,1}
             },
             {
-            	//
-        		//
-        		// TODO: Implement pentomino representation F
-        		//
-        		//
+                    {0,1,1},
+                    {1,1,0},
+                    {0,1,0}
             }
     };
 
@@ -136,7 +130,7 @@ public class PentominoBuilder {
     }
 
     /**
-     * Rotate the matrix x times over 90 degrees 
+     * Rotate the matrix x times over 90 degrees
      * Assume that the matrix is a square!
      * It does not make a copy, so the return matrix does not have to be used
      * @param data: a matrix
@@ -376,14 +370,7 @@ public class PentominoBuilder {
      */
     public static boolean isEqual(int[][] data1, int[][] data2)
     {
-    	
-    	//
-    	//
-    	// TODO: To be implemented
-    	//
-    	//
-
-        return false;
+        return Arrays.deepEquals(data1,data2);
     }
 
 
@@ -446,8 +433,8 @@ public class PentominoBuilder {
         return returnData;
     }
 
-    
-    /** 
+
+    /**
      * @param args
      * @throws FileNotFoundException
      * @throws UnsupportedEncodingException
@@ -475,6 +462,10 @@ public class PentominoBuilder {
             }
         }
         writer.close();
+    }
+
+    public static int[][][] getBasicDatabase (){
+        return basicDatabase;
     }
 
 }
--- src/Search.java
+++ src/Search.java
@@ -3,77 +3,85 @@
  * @version 2022.0
  */
 
- import java.util.Random;
+import java.util.Random;
 
 /**
  * This class includes the methods to support the search of a solution.
  */
 public class Search
 {
-    public static final int horizontalGridSize = 5;
-    public static final int verticalGridSize = 6;
-    
-    public static final char[] input = { 'W', 'Y', 'I', 'T', 'Z', 'L'};
-    
-    //Static UI class to display the board
-    public static UI ui = new UI(horizontalGridSize, verticalGridSize, 50);
+	public static final int horizontalGridSize = 5;
+	public static int count = 0;
+	public static final int verticalGridSize = 6;
+
+	public static final char[] input = {'W', 'Y', 'I', 'T', 'Z', 'L'};
+
+	//	{'X', 'I', 'Z', 'T', 'U', 'V', 'W', 'Y', 'L', 'P', 'N', 'F'}
+
+	//Static UI class to display the board
+	public static UI ui = new UI(horizontalGridSize, verticalGridSize, 50);
 
 	/**
 	 * Helper function which starts a basic search algorithm
 	 */
-    public static void search()
-    {
-        // Initialize an empty board
-        int[][] field = new int[horizontalGridSize][verticalGridSize];
+	public static void search()
+	{
+		// Initialize an empty board
+		int[][] field = new int[horizontalGridSize][verticalGridSize];
 
-        for(int i = 0; i < field.length; i++)
-        {
-            for(int j = 0; j < field[i].length; j++)
-            {
-                // -1 in the state matrix corresponds to empty square
-                // Any positive number identifies the ID of the pentomino
-            	field[i][j] = -1;
-            }
-        }
-        //Start the basic search
-        basicSearch(field);
-    }
-	
+		for(int i = 0; i < field.length; i++)
+		{
+			for(int j = 0; j < field[i].length; j++)
+			{
+				// -1 in the state matrix corresponds to empty square
+				// Any positive number identifies the ID of the pentomino
+				field[i][j] = -1;
+			}
+		}
+		//Start the basic search
+		if (basicSearch(field, 0)) {
+			System.out.println("Solution Found");
+		} else {
+			System.out.println("Has No Solution");
+		}
+		ui.setState(field);
+	}
+
 	/**
 	 * Get as input the character representation of a pentomino and translate it into its corresponding numerical value (ID)
 	 * @param character a character representating a pentomino
 	 * @return	the corresponding ID (numerical value)
 	 */
-    private static int characterToID(char character) {
-    	int pentID = -1; 
-    	if (character == 'X') {
-    		pentID = 0;
-    	} else if (character == 'I') {
-    		pentID = 1;
-    	} else if (character == 'Z') {
-    		pentID = 2;
-    	} else if (character == 'T') {
-    		pentID = 3;
-    	} else if (character == 'U') {
-    		pentID = 4;
-     	} else if (character == 'V') {
-     		pentID = 5;
-     	} else if (character == 'W') {
-     		pentID = 6;
-     	} else if (character == 'Y') {
-     		pentID = 7;
-    	} else if (character == 'L') {
-    		pentID = 8;
-    	} else if (character == 'P') {
-    		pentID = 9;
-    	} else if (character == 'N') {
-    		pentID = 10;
-    	} else if (character == 'F') {
-    		pentID = 11;
-    	} 
-    	return pentID;
-    }
-	
+	private static int characterToID(char character) {
+		int pentID = -1;
+		if (character == 'X') {
+			pentID = 0;
+		} else if (character == 'I') {
+			pentID = 1;
+		} else if (character == 'Z') {
+			pentID = 2;
+		} else if (character == 'T') {
+			pentID = 3;
+		} else if (character == 'U') {
+			pentID = 4;
+		} else if (character == 'V') {
+			pentID = 5;
+		} else if (character == 'W') {
+			pentID = 6;
+		} else if (character == 'Y') {
+			pentID = 7;
+		} else if (character == 'L') {
+			pentID = 8;
+		} else if (character == 'P') {
+			pentID = 9;
+		} else if (character == 'N') {
+			pentID = 10;
+		} else if (character == 'F') {
+			pentID = 11;
+		}
+		return pentID;
+	}
+
 	/**
 	 * Basic implementation of a search algorithm. It is not a bruto force algorithms (it does not check all the posssible combinations)
 	 * but randomly takes possible combinations and positions to find a possible solution.
@@ -81,77 +89,85 @@ public class Search
 	 * This algorithm can be very time-consuming
 	 * @param field a matrix representing the board to be fulfilled with pentominoes
 	 */
-    private static void basicSearch(int[][] field){
-    	Random random = new Random();
-    	boolean solutionFound = false;
-    	
-    	while (!solutionFound) {
-    		solutionFound = true;
-    		
-    		//Empty board again to find a solution
-			for (int i = 0; i < field.length; i++) {
-				for (int j = 0; j < field[i].length; j++) {
-					field[i][j] = -1;
+	private static boolean basicSearch(int[][] field, int index){
+		count++;
+		if (index == input.length) {
+			return true;
+		}
+		int id = characterToID(input[index]);
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[0].length; j++) {
+				for (int k = 0; k < PentominoDatabase.data[id].length; k++) {
+					int[][] piece = PentominoDatabase.data[id][k];
+
+					if (canPlace(piece, i, j, field)) {
+						addPiece(field, piece, id, i, j);
+						ui.setState(field);
+//						try {
+//							Thread.sleep(10);
+//						} catch (Exception ex) {
+//							System.out.println(ex);
+//						}
+
+
+						if (basicSearch(field, index + 1)) {
+							return true;
+						}
+
+						removePiece(field, piece, i, j);
+					}
 				}
 			}
-    		
-    		//Put all pentominoes with random rotation/flipping on a random position on the board
-    		for (int i = 0; i < input.length; i++) {
-    			
-    			//Choose a pentomino and randomly rotate/flip it
-    			int pentID = characterToID(input[i]);
-    			int mutation = random.nextInt(PentominoDatabase.data[pentID].length);
-    			int[][] pieceToPlace = PentominoDatabase.data[pentID][mutation];
-    		
-    			//Randomly generate a position to put the pentomino on the board
-    			int x;
-    			int y;
-    			if (horizontalGridSize < pieceToPlace.length) {
-    				//this particular rotation of the piece is too long for the field
-    				x=-1;
-    			} else if (horizontalGridSize == pieceToPlace.length) {
-    				//this particular rotation of the piece fits perfectly into the width of the field
-    				x = 0;
-    			} else {
-    				//there are multiple possibilities where to place the piece without leaving the field
-    				x = random.nextInt(horizontalGridSize-pieceToPlace.length+1);
-    			}
+		}
 
-    			if (verticalGridSize < pieceToPlace[0].length) {
-    				//this particular rotation of the piece is too high for the field
-    				y=-1;
-    			} else if (verticalGridSize == pieceToPlace[0].length) {
-    				//this particular rotation of the piece fits perfectly into the height of the field
-    				y = 0;
-    			} else {
-    				//there are multiple possibilities where to place the piece without leaving the field
-    				y = random.nextInt(verticalGridSize-pieceToPlace[0].length+1);
-    			}
-    		
-    			//If there is a possibility to place the piece on the field, do it
-    			if (x >= 0 && y >= 0) {
-	    			addPiece(field, pieceToPlace, pentID, x, y);
-	    		} 
-    		}
-    		//Check whether complete field is filled
-    		//
-    		//
-    		// TODO: To be implemented
-    		//
-    		//
-    		
 
-    		
-    		if (solutionFound) {
-    			//display the field
-    			ui.setState(field); 
-    			System.out.println("Solution found");
-    			break;
-    		}
-    	}
-    }
+		return false;
+	}
 
-    
+	public static boolean canPlace(int[][] piece, int row, int col, int[][] field) {
+		for (int i = 0; i < piece.length; i++) {
+			for (int j = 0; j < piece[0].length; j++) {
+				if (piece[i][j] != 0) {
+					int newRow = row + i;
+					int newCol = col + j;
+					if (newRow < 0 || newRow >= field.length || newCol < 0 || newCol >= field[0].length || field[newRow][newCol] != -1) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+
+//		if (row + piece.length > field.length) {
+//			return false;
+//		}
+//		for (int i = 0; i < piece.length; i++) {
+//			if (col + piece[i].length > field[0].length)
+//				return false;
+//		}
+//
+//		for (int i = 0; i < piece.length; i++) {
+//			for (int j = 0; j < piece[i].length; j++) {
+//				if (piece[i][j] >= 1 && field[row + i][col + j] != -1) {
+//					return false;
+//				}
+//			}
+//		}
+//
+//		return true;
+	}
+
+	public static void removePiece(int[][] field, int[][] piece, int row, int col) {
+		for (int i = 0; i < piece.length; i++) {
+			for (int j = 0; j < piece[0].length; j++) {
+				if (piece[i][j] != 0) {
+					field[row + i][col + j] = -1;
+				}
+			}
+		}
+	}
+
+
 	/**
 	 * Adds a pentomino to the position on the field (overriding current board at that position)
 	 * @param field a matrix representing the board to be fulfilled with pentominoes
@@ -160,26 +176,28 @@ public class Search
 	 * @param x x position of the pentomino
 	 * @param y y position of the pentomino
 	 */
-    public static void addPiece(int[][] field, int[][] piece, int pieceID, int x, int y)
-    {
-        for(int i = 0; i < piece.length; i++) // loop over x position of pentomino
-        {
-            for (int j = 0; j < piece[i].length; j++) // loop over y position of pentomino
-            {
-                if (piece[i][j] == 1)
-                {
-                    // Add the ID of the pentomino to the board if the pentomino occupies this square
-                    field[x + i][y + j] = pieceID;
-                }
-            }
-        }
-    }
+	public static void addPiece(int[][] field, int[][] piece, int pieceID, int x, int y)
+	{
+		for(int i = 0; i < piece.length; i++) // loop over x position of pentomino
+		{
+			for (int j = 0; j < piece[i].length; j++) // loop over y position of pentomino
+			{
+				if (piece[i][j] == 1)
+				{
+					// Add the ID of the pentomino to the board if the pentomino occupies this square
+					field[x + i][y + j] = pieceID;
+				}
+			}
+		}
+	}
 
 	/**
 	 * Main function. Needs to be executed to start the basic search algorithm
 	 */
-    public static void main(String[] args)
-    {
-        search();
-    }
+	public static void main(String[] args)
+	{
+		PentominoBuilder.makeDatabase();
+		search();
+		System.out.print("Number of iterations: " + count);
+	}
 }
