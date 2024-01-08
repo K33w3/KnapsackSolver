b130d0713bc674f8b84e7cdb45c2de506517e8e1
I6365974 <a.visoiu@student.maastrichtuniversity.nl>
2024-01-08 16:26:28 +0100

    Testing Genetic Algorithm branch

--- knapsack/src/main/java/com/bcs2023/Main.java
+++ knapsack/src/main/java/com/bcs2023/Main.java
@@ -1,7 +1,9 @@
 package com.bcs2023;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Genetic Algorithm test");
+  }
 }
