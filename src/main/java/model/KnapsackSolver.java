4136ca76f37e235141df5e6f7d96a722e25dc776
Rallis, Vasileios (Stud. DACS) <v.rallis@student.maastrichtuniversity.nl>
2024-01-08 14:21:30 +0000

    removed everything apart from the zip file

--- src/main/java/model/KnapsackSolver.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.List;
-
-public class KnapsackSolver {
-    public void solve(CargoSpace cargoSpace, List<Parcel> parcels) {
-        // Implement the algorithm logic here
-    }
-}
