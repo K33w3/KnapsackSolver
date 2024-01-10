20e111e082280a930c54071df33e74b0061403b5
I6365974 <a.visoiu@student.maastrichtuniversity.nl>
2024-01-10 17:56:22 +0100

    Added GreedyKnapsack algorithm parameters

--- src/main/java/com/bcs2024/knapsack/model/CargoSpace.java
+++ src/main/java/com/bcs2024/knapsack/model/CargoSpace.java
@@ -6,87 +6,95 @@ import java.util.Arrays;
  * Represents the cargo space with dimensions and tracking for occupied space.
  */
 public class CargoSpace {
-    private double length, width, height;
-    private final boolean[][][] occupied;
 
-    public double getLength() {
-        return length;
-    }
+  private double length, width, height;
+  private final boolean[][][] occupied;
 
-    public void setLength(double length) {
-        this.length = length;
-    }
+  public double getLength() {
+    return length;
+  }
 
-    public double getWidth() {
-        return width;
-    }
+  public void setLength(double length) {
+    this.length = length;
+  }
 
-    public void setWidth(double width) {
-        this.width = width;
-    }
+  public double getWidth() {
+    return width;
+  }
 
-    public double getHeight() {
-        return height;
-    }
+  public void setWidth(double width) {
+    this.width = width;
+  }
 
-    public void setHeight(double height) {
-        this.height = height;
-    }
+  public double getHeight() {
+    return height;
+  }
 
-    /**
-     * Constructs a new CargoSpace with specified dimensions.
-     *
-     * @param length The length of the cargo space.
-     * @param width  The width of the cargo space.
-     * @param height The height of the cargo space.
-     */
-    public CargoSpace(double length, double width, double height) {
-        this.length = length;
-        this.width = width;
-        this.height = height;
-        this.occupied = new boolean[(int) length][(int) width][(int) height];
-    }
+  public void setHeight(double height) {
+    this.height = height;
+  }
 
-    /**
-     * Checks if a specific area of the cargo space is available to occupy based on the given parcel placement.
-     *
-     * @param placement The ParcelPlacement object representing the parcel's placement in the cargo space.
-     * @return true if the space is available, false otherwise.
-     */
-    public boolean isSpaceAvailable(ParcelPlacement placement) {
-        Parcel parcel = placement.getParcel();
-        int orientation = placement.getOrientation();
+  /**
+   * Constructs a new CargoSpace with specified dimensions.
+   *
+   * @param length The length of the cargo space.
+   * @param width  The width of the cargo space.
+   * @param height The height of the cargo space.
+   */
+  public CargoSpace(double length, double width, double height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    this.occupied = new boolean[2 * length][2 * width][2 * height];
+  }
 
-        // Get the shape of the parcel based on its orientation
-        boolean[][][] parcelShape = getParcelShape(parcel, orientation);
+  /**
+   * Checks if a specific area of the cargo space is available to occupy based on the given parcel placement.
+   *
+   * @param placement The ParcelPlacement object representing the parcel's placement in the cargo space.
+   * @return true if the space is available, false otherwise.
+   */
+  public boolean isSpaceAvailable(ParcelPlacement placement) {
+    Parcel parcel = placement.getParcel();
+    int orientation = placement.getOrientation();
 
-        int startX = placement.getX();
-        int startY = placement.getY();
-        int startZ = placement.getZ();
+    // Get the shape of the parcel based on its orientation
+    boolean[][][] parcelShape = getParcelShape(parcel, orientation);
 
-        for (int i = 0; i < parcelShape.length; i++) {
-            for (int j = 0; j < parcelShape[i].length; j++) {
-                for (int k = 0; k < parcelShape[i][j].length; k++) {
-                    if (parcelShape[i][j][k]) {
-                        int x = startX + i;
-                        int y = startY + j;
-                        int z = startZ + k;
-                        if (x >= this.length || y >= this.width || z >= this.height || occupied[x][y][z]) {
-                            return false;
-                        }
-                    }
-                }
+    int startX = placement.getX();
+    int startY = placement.getY();
+    int startZ = placement.getZ();
+
+    for (int i = 0; i < parcelShape.length; i++) {
+      for (int j = 0; j < parcelShape[i].length; j++) {
+        for (int k = 0; k < parcelShape[i][j].length; k++) {
+          if (parcelShape[i][j][k]) {
+            int x = startX + i;
+            int y = startY + j;
+            int z = startZ + k;
+            if (
+              x >= this.length ||
+              y >= this.width ||
+              z >= this.height ||
+              occupied[x][y][z]
+            ) {
+              return false;
             }
+          }
         }
-        return true;
+      }
     }
+    return true;
+  }
 
-    private boolean[][][] getParcelShape(Parcel parcel, int orientation) {
-            boolean[][][][] rotations = ShapesAndRotations.getRotations(parcel.getType());
-            return rotations[orientation % rotations.length];
-    }
+  private boolean[][][] getParcelShape(Parcel parcel, int orientation) {
+    boolean[][][][] rotations = ShapesAndRotations.getRotations(
+      parcel.getType()
+    );
+    return rotations[orientation % rotations.length];
+  }
 
-    /*public int[] getAdjustedDimensions(Parcel parcel, int orientation) {
+  /*public int[] getAdjustedDimensions(Parcel parcel, int orientation) {
         boolean[][][][] rotations = switch (parcel.getType()) {
             case "L" -> ShapesAndRotations.getL();
             case "P" -> ShapesAndRotations.getP();
@@ -110,45 +118,54 @@ public class CargoSpace {
         return new int[]{length, width, height};
     }*/
 
-    /**
-     * Marks a specific area of the cargo space as occupied based on the given parcel placement.
-     * This method uses the coordinates and dimensions from the ParcelPlacement object to determine
-     * the space that the parcel will occupy. It updates the occupied array accordingly.
-     *
-     * @param placement The ParcelPlacement object representing the parcel's placement in the cargo space.
-     */
-    public void occupySpace(ParcelPlacement placement) {
-        Parcel parcel = placement.getParcel();
-        int orientation = placement.getOrientation();
+  /**
+   * Marks a specific area of the cargo space as occupied based on the given parcel placement.
+   * This method uses the coordinates and dimensions from the ParcelPlacement object to determine
+   * the space that the parcel will occupy. It updates the occupied array accordingly.
+   *
+   * @param placement The ParcelPlacement object representing the parcel's placement in the cargo space.
+   */
+  public void occupySpace(ParcelPlacement placement) {
+    Parcel parcel = placement.getParcel();
+    int orientation = placement.getOrientation();
 
-        // Get the shape of the parcel based on its orientation
-        boolean[][][] parcelShape = getParcelShape(parcel, orientation);
+    // Get the shape of the parcel based on its orientation
+    boolean[][][] parcelShape = getParcelShape(parcel, orientation);
 
-        int startX = placement.getX();
-        int startY = placement.getY();
-        int startZ = placement.getZ();
+    int startX = placement.getX();
+    int startY = placement.getY();
+    int startZ = placement.getZ();
 
-        for (int i = 0; i < parcelShape.length; i++) {
-            for (int j = 0; j < parcelShape[i].length; j++) {
-                for (int k = 0; k < parcelShape[i][j].length; k++) {
-                    if (parcelShape[i][j][k]) {
-                        int x = startX + i;
-                        int y = startY + j;
-                        int z = startZ + k;
-                        occupied[x][y][z] = true;
-                    }
-                }
-            }
+    for (int i = 0; i < parcelShape.length; i++) {
+      for (int j = 0; j < parcelShape[i].length; j++) {
+        for (int k = 0; k < parcelShape[i][j].length; k++) {
+          if (parcelShape[i][j][k]) {
+            int x = startX + i;
+            int y = startY + j;
+            int z = startZ + k;
+            occupied[x][y][z] = true;
+          }
         }
+      }
     }
+  }
 
-    public void printOccupiedSpacePositions() {
-        for (int i = 0; i < occupied.length; i++) {
-            for (int j = 0; j < occupied[i].length; j++) {
-                for (int k = 0; k < occupied[i][j].length; k++) {
-                    System.out.println("Position (" + i + ", " + j + ", " + k + "): " + (occupied[i][j][k] ? "Occupied" : "Free"));
-                }
-            }
+  public void printOccupiedSpacePositions() {
+    for (int i = 0; i < occupied.length; i++) {
+      for (int j = 0; j < occupied[i].length; j++) {
+        for (int k = 0; k < occupied[i][j].length; k++) {
+          System.out.println(
+            "Position (" +
+            i +
+            ", " +
+            j +
+            ", " +
+            k +
+            "): " +
+            (occupied[i][j][k] ? "Occupied" : "Free")
+          );
         }
+      }
     }
+  }
 }
