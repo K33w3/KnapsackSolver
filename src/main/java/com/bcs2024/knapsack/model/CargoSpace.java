303e06b7943aa9c44bc3a59b18e5f199cc1e15f8
I6365974 <a.visoiu@student.maastrichtuniversity.nl>
2024-01-12 17:22:05 +0100

    Add working solver for the cargo space

--- src/main/java/com/bcs2024/knapsack/model/CargoSpace.java
+++ src/main/java/com/bcs2024/knapsack/model/CargoSpace.java
@@ -1,80 +1,50 @@
 package com.bcs2024.knapsack.model;
 
-/**
- * Represents the cargo space with dimensions and tracking for occupied space.
- */
+import java.util.Arrays;
+
 public class CargoSpace {
 
-  private double length, width, height;
-  private final boolean[][][] occupied;
+  private final double length = 16.5;
+  private final double width = 2.5;
+  private final double height = 4;
+  private int[][][] cargoMatrix;
+  private ShapesAndRotations shapes;
 
-  public double getLength() {
-    return length;
+  public CargoSpace() {
+    shapes = new ShapesAndRotations();
+    int x = (int) (width * 2);
+    int y = (int) (height * 2);
+    int z = (int) (length * 2);
+    cargoMatrix = new int[x][y][z];
+
+    for (int i = 0; i < x; i++) {
+      for (int j = 0; j < y; j++) {
+        for (int k = 0; k < z; k++) {
+          cargoMatrix[i][j][k] = -1;
+        }
+      }
+    }
   }
 
-  public void setLength(double length) {
-    this.length = length;
-  }
-
-  public double getWidth() {
-    return width;
-  }
-
-  public void setWidth(double width) {
-    this.width = width;
-  }
-
-  public double getHeight() {
-    return height;
-  }
-
-  public void setHeight(double height) {
-    this.height = height;
-  }
-
-  /**
-   * Constructs a new CargoSpace with specified dimensions.
-   *
-   * @param length The length of the cargo space.
-   * @param width  The width of the cargo space.
-   * @param height The height of the cargo space.
-   */
-  public CargoSpace(double length, double width, double height) {
-    this.length = length;
-    this.width = width;
-    this.height = height;
-    this.occupied = new boolean[2 * length][2 * width][2 * height];
-  }
-
-  /**
-   * Checks if a specific area of the cargo space is available to occupy based on the given parcel placement.
-   *
-   * @param placement The ParcelPlacement object representing the parcel's placement in the cargo space.
-   * @return true if the space is available, false otherwise.
-   */
-  public boolean isSpaceAvailable(ParcelPlacement placement) {
-    Parcel parcel = placement.getParcel();
-    int orientation = placement.getOrientation();
-
-    // Get the shape of the parcel based on its orientation
-    boolean[][][] parcelShape = getParcelShape(parcel, orientation);
-
-    int startX = placement.getX();
-    int startY = placement.getY();
-    int startZ = placement.getZ();
-
-    for (int i = 0; i < parcelShape.length; i++) {
-      for (int j = 0; j < parcelShape[i].length; j++) {
-        for (int k = 0; k < parcelShape[i][j].length; k++) {
-          if (parcelShape[i][j][k]) {
-            int x = startX + i;
+  public boolean canPlace(
+    int[][][] shape,
+    int startX,
+    int startY,
+    int startZ,
+    int[][][] destination
+  ) {
+    for (int i = 0; i < shape.length; i++) {
+      for (int j = 0; j < shape[0].length; j++) {
+        for (int k = 0; k < shape[0][0].length; k++) {
+          if (shape[i][j][k] != 0) {
+            int x = startX + k;
             int y = startY + j;
-            int z = startZ + k;
+            int z = startZ + i;
             if (
-              x >= this.length ||
-              y >= this.width ||
-              z >= this.height ||
-              occupied[x][y][z]
+              x >= (int) (this.width * 2) ||
+              y >= (int) (this.height * 2) ||
+              z >= (int) (this.length * 2) ||
+              destination[x][y][z] != -1
             ) {
               return false;
             }
@@ -85,85 +55,40 @@ public class CargoSpace {
     return true;
   }
 
-  private boolean[][][] getParcelShape(Parcel parcel, int orientation) {
-    boolean[][][][] rotations = ShapesAndRotations.getRotations(
-      parcel.getType()
-    );
-    return rotations[orientation % rotations.length];
-  }
-
-  /*public int[] getAdjustedDimensions(Parcel parcel, int orientation) {
-        boolean[][][][] rotations = switch (parcel.getType()) {
-            case "L" -> ShapesAndRotations.getL();
-            case "P" -> ShapesAndRotations.getP();
-            case "T" -> ShapesAndRotations.getT();
-            case "A" -> ShapesAndRotations.getA();
-            case "B" -> ShapesAndRotations.getB();
-            case "C" -> ShapesAndRotations.getC();
-            default -> throw new IllegalArgumentException("Invalid parcel type");
-        };
-
-        // Select the correct shape and its rotations based on the parcel type
-
-        // Use the orientation to select the correct rotation
-        boolean[][][] selectedRotation = rotations[orientation % rotations.length];
-
-        // Calculate dimensions based on the selected rotation
-        int length = selectedRotation.length;
-        int width = selectedRotation[0].length;
-        int height = selectedRotation[0][0].length;
-
-        return new int[]{length, width, height};
-    }*/
-
-  /**
-   * Marks a specific area of the cargo space as occupied based on the given parcel placement.
-   * This method uses the coordinates and dimensions from the ParcelPlacement object to determine
-   * the space that the parcel will occupy. It updates the occupied array accordingly.
-   *
-   * @param placement The ParcelPlacement object representing the parcel's placement in the cargo space.
-   */
-  public void occupySpace(ParcelPlacement placement) {
-    Parcel parcel = placement.getParcel();
-    int orientation = placement.getOrientation();
-
-    // Get the shape of the parcel based on its orientation
-    boolean[][][] parcelShape = getParcelShape(parcel, orientation);
-
-    int startX = placement.getX();
-    int startY = placement.getY();
-    int startZ = placement.getZ();
-
-    for (int i = 0; i < parcelShape.length; i++) {
-      for (int j = 0; j < parcelShape[i].length; j++) {
-        for (int k = 0; k < parcelShape[i][j].length; k++) {
-          if (parcelShape[i][j][k]) {
-            int x = startX + i;
+  public void placeParcel(
+    int[][][] shape,
+    int startX,
+    int startY,
+    int startZ,
+    int[][][] destination
+  ) {
+    for (int i = 0; i < shape.length; i++) {
+      for (int j = 0; j < shape[0].length; j++) {
+        for (int k = 0; k < shape[0][0].length; k++) {
+          if (shape[i][j][k] != 0) {
+            int x = startX + k;
             int y = startY + j;
-            int z = startZ + k;
-            occupied[x][y][z] = true;
+            int z = startZ + i;
+            destination[x][y][z] = shape[i][j][k];
           }
         }
       }
     }
   }
 
-  public void printOccupiedSpacePositions() {
-    for (int i = 0; i < occupied.length; i++) {
-      for (int j = 0; j < occupied[i].length; j++) {
-        for (int k = 0; k < occupied[i][j].length; k++) {
-          System.out.println(
-            "Position (" +
-            i +
-            ", " +
-            j +
-            ", " +
-            k +
-            "): " +
-            (occupied[i][j][k] ? "Occupied" : "Free")
-          );
-        }
-      }
-    }
+  public double getLength() {
+    return length;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public int[][][] getCargoMatric() {
+    return this.cargoMatrix;
   }
 }
--- src/main/java/com/bcs2024/knapsack/model/Parcel.java
+++ src/main/java/com/bcs2024/knapsack/model/Parcel.java
@@ -1,20 +1,16 @@
 package com.bcs2024.knapsack.model;
 
-/**
- * Represents a parcel with specific dimensions and value.
- * Each parcel has a type, length, width, height, and an associated value.
- */
 public class Parcel {
 
   private double length, width, height, value;
   private String type;
+  private int[][][] shape;
+  private ShapesAndRotations shapes;
 
-  private boolean[][][] shape; // 3D array to represent complex shapes
-
-  // Constructor for parcels (A, B, C, L, P, T)
-  public Parcel(final String type, boolean[][][] shape) {
+  public Parcel(String type) {
+    shapes = new ShapesAndRotations();
     this.type = type;
-    this.shape = shape;
+    this.shape = shapes.getShape(type, 0);
     initializeParcel();
   }
 
@@ -24,146 +20,55 @@ public class Parcel {
         this.length = 1.0;
         this.width = 1.0;
         this.height = 2.0;
-        this.value = 1;
+        this.value = 3;
       }
       case "B" -> {
         this.length = 1.0;
         this.width = 1.5;
         this.height = 2.0;
-        this.value = 2;
+        this.value = 4;
       }
       case "C" -> {
         this.length = 1.5;
         this.width = 1.5;
         this.height = 1.5;
-        this.value = 3;
+        this.value = 5;
       }
       case "L", "P", "T" -> {
         this.length = 0.5;
         this.width = 0.5;
         this.height = 0.5;
-        // Set value based on type or other criteria
-        this.value = determineValueForComplexParcel();
+        this.value = 0;
       }
       default -> System.out.println("Invalid type");
     }
   }
 
-  // TODO create logic for determining value of complex parcels
-  private double determineValueForComplexParcel() {
-    return 0;
+  public double getVolume(String type) {
+    return this.length * this.height * this.width;
   }
 
-  /**
-   * Returns the shape of the parcel.
-   *
-   * @return The shape of the parcel.
-   */
-  public boolean[][][] getShape() {
+  public int[][][] getShape() {
     return shape;
   }
 
-  /**
-   * Sets the shape of the parcel.
-   *
-   * @param shape The new shape of the parcel.
-   */
-  public void setShape(boolean[][][] shape) {
-    this.shape = shape;
-  }
-
-  /**
-   * Returns the length of the parcel.
-   *
-   * @return The length of the parcel.
-   */
   public double getLength() {
     return length;
-  }
-
-  /**
-   * Sets the length of the parcel.
-   *
-   * @param length The new length of the parcel.
-   */
-  public void setLength(final double length) {
-    this.length = length;
-  }
-
-  /**
-
-     * Sets the shape of the parcel with its orientation.
-     *
-     * @param shape The new shape of the parcel.
-     */
-  public void setShape(boolean[][][][] shape) {
-    this.shape = shape;
   }
 
   public double getWidth() {
     return width;
   }
 
-  /**
-   * Sets the width of the parcel.
-   *
-   * @param width The new width of the parcel.
-   */
-  public void setWidth(final double width) {
-    this.width = width;
-  }
-
-  /**
-   * Returns the height of the parcel.
-   *
-   * @return The height of the parcel.
-   */
   public double getHeight() {
     return height;
   }
 
-  /**
-   * Sets the height of the parcel.
-   *
-   * @param height The new height of the parcel.
-   */
-  public void setHeight(final double height) {
-    this.height = height;
-  }
-
-  /**
-   * Returns the value of the parcel.
-   *
-   * @return The value of the parcel.
-   */
   public double getValue() {
     return value;
   }
 
-  /**
-   * Sets the value of the parcel.
-   *
-   * @param value The new value of the parcel.
-   */
-  public void setValue(final double value) {
-    this.value = value;
-  }
-
-  /**
-   * Returns the type of the parcel.
-   *
-   * @return The type of the parcel.
-   */
   public String getType() {
     return type;
-  }
-
-  /**
-   * Sets the type of the parcel.
-   *
-   * @param type The new type of the parcel.
-   */
-  public void setType(final String type) {
-    this.type = type;
   }
 }
