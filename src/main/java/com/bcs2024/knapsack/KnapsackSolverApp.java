27affcf2d87545db6a019aa7d24768876cbcc6b9
JackOUT <jack.bikar@gmail.com>
2024-01-13 19:27:41 +0100

    Removed unnecessary code

--- src/main/java/com/bcs2024/knapsack/KnapsackSolverApp.java
+++ src/main/java/com/bcs2024/knapsack/KnapsackSolverApp.java
@@ -10,33 +10,6 @@ import java.util.List;
 public class KnapsackSolverApp {
 
   public static void main(String[] args) {
-    // Initialize the cargo space
-    CargoSpace cargoSpace = new CargoSpace(16.5, 2.5, 4.0);
 
-    // Initialize parcels
-    List<Parcel> parcels = new ArrayList<>();
-    parcels.add(new Parcel("A", 1.0, 1.0, 2.0, 1));
-    parcels.add(new Parcel("B", 1.0, 1.5, 2.0, 2));
-    parcels.add(new Parcel("C", 1.5, 1.5, 1.5, 3));
-
-    // Choose and apply the solving strategy
-    KnapsackSolverStrategy solver;
-
-    // TODO add logic to choose the solving strategy in GUI
-
-    System.out.println("Solving using Greedy Algorithm");
-    solver = new GreedyKnapsackSolver();
-    solver.solve(cargoSpace, parcels);
-    // Display the result or analysis of the solution
-    cargoSpace.printOccupiedSpacePositions();
-
-    // Reset cargoSpace for next algorithm
-    cargoSpace = new CargoSpace(16.5, 2.5, 4.0);
-
-    System.out.println("Solving using Genetic Algorithm");
-    solver = new GeneticKnapsackSolver();
-    solver.solve(cargoSpace, parcels);
-    // Display the result or analysis of the solution
-    cargoSpace.printOccupiedSpacePositions();
   }
 }
